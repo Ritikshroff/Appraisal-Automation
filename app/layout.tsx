f3b7738a3e73e93e182f2d7bac667048c30a0ef7
Ritikkumar88 <chomu4321ritik@gmail.com>
2026-03-30 14:33:51 +0530

    tsting head added

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning={true}>
       <head>
-        <Script
-          src="https://cmg-backend.s3.eu-north-1.amazonaws.com/static/js/finserve_uae.min.js"
-          strategy="afterInteractive"
-        />
+        <script type="text/javascript" src="https://cmg-backend.s3.eu-north-1.amazonaws.com/static/js/test_account.min.js" defer></script>
       </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning={true}>
         {children}
